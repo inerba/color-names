61c1ff80c326a2c730eb66976bfe273b1d980f7a
Francesco Apruzzese <inerba@gmail.com>
2018-04-20 23:09:56 +0200

    .

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,12 @@ var rename      = require('gulp-rename');
 var exorcist    = require('exorcist');
 var browserify  = require('browserify');
 var browserSync = require('browser-sync').create();
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var concat = require("gulp-concat");
+var cleanCss = require('gulp-clean-css');
 
 // Watchify args contains necessary cache options to achieve fast incremental bundles.
 // See watchify readme for details. Adding debug true for source-map generation.
@@ -46,6 +52,33 @@ gulp.task('minify', ['bundle'], function () {
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest("./app/js/dist"));
 });
+
+gulp.task('styles', function () {
+    return gulp.src([
+        'src/_scss/*.scss'
+    ])
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss([autoprefixer({
+            browsers: [
+                'Chrome >= 35',
+                'Firefox >= 38',
+                'Edge >= 12',
+                'Explorer >= 10',
+                'iOS >= 8',
+                'Safari >= 8',
+                'Android 2.3',
+                'Android >= 4',
+                'Opera >= 12']
+        })]))
+        .pipe(concat("style.css"))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('assets/css/'))
+        .pipe(cleanCss())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('assets/css/'))
+        .pipe(browserSync.stream());
+});
 /**
  * Gulp task alias
  */
@@ -57,10 +90,14 @@ gulp.task('copy', function () {
     gulp.src("node_modules/color-name-list/dist/*").pipe(gulp.dest('assets/vendor/color-name-list/'));
 });
 
+gulp.task('watch', ['styles'], function () {
+    gulp.watch(['src/_scss/**/*.scss'], ['styles']);
+});
+
 /**
  * First bundle, then serve from the ./app directory
  */
-gulp.task('default', ['bundle'], function () {
+gulp.task('default', ['bundle','watch'], function () {
     browserSync.init({
         server: "./"
     });
